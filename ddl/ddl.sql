5a3093f515f31114074282ee2e672fa60a81c132
Sanket <sanketgupta1000@gmail.com>
2023-09-18 09:08:41 +0530

    Added initial files with complete ddl, some dml, and the models representing database

---
+++ ddl/ddl.sql
@@ -0,0 +1,109 @@
+CREATE TABLE Person
+(
+	Person_ID varchar(10) PRIMARY KEY,
+    F_Name varchar(50) not null,
+    M_Name varchar(50) not null,
+    L_Name varchar(50) not null,
+    House_No varchar(10) not null,
+    society_name varchar(50) not null,
+    street_name varchar(50) not null,
+    locality varchar(30) not null,
+    pincode number(6,0) not null,
+    gender varchar(10) CHECK (gender IN('Male', 'Female', 'Other')),
+    is_student number(1, 0) check (is_student in(0, 1)),
+    dob date not null
+);
+
+CREATE TABLE person_mobile(
+	person_id varchar(10),
+    mobile_no number(10, 0) not null,
+    foreign key(person_id) REFERENCES person(person_id),
+    primary key(person_id, mobile_no)
+);
+
+CREATE TABLE student(
+	student_id varchar(10) PRIMARY KEY,
+    person_id varchar(10),
+    foreign key(student_id) REFERENCES person(person_id)
+);
+
+CREATE TABLE employee(
+	employee_id varchar(10) PRIMARY KEY,
+    person_id varchar(10),
+    salary number(6, 0) not null,
+    job varchar(30) check (job in('Teacher', 'Watchman', 'Cleanliness Worker', 'Peon')),
+    foreign key (person_id) REFERENCES person(person_id)
+);
+
+CREATE TABLE teacher(
+	teacher_id varchar(10) PRIMARY KEY,
+    employee_id varchar(10),
+    foreign key (employee_id) REFERENCES employee(employee_id)
+);
+
+CREATE TABLE classroom(
+	room_no number(3, 0),
+    building_no number(3, 0),
+    capacity number(3, 0),
+    primary key(room_no, building_no)
+);
+
+CREATE TABLE standard(
+	standard_no number(2, 0) PRIMARY KEY,
+    teacher_id varchar(10),
+    room_no number(3, 0),
+    building_no number(3, 0),
+    foreign key(teacher_id) REFERENCES teacher(teacher_id),
+    foreign key(room_no, building_no) references classroom
+);
+
+create table subject
+(
+    subject_name varchar(30) not null,
+    standard_no number(3),
+    book varchar(40) not null,
+    foreign key(standard_no) references standard(standard_no),
+    primary key(subject_name, standard_no)
+);
+
+create table enrollment
+(
+    roll_no number(3),
+    standard_no number(3),
+    student_id varchar(10) not null,
+    enrolled_date date not null,
+    foreign key(standard_no) references standard,
+    foreign key(student_id) references student,
+    primary key(roll_no, standard_no)
+);
+
+create table lecture
+(
+    lecture_no number(2),
+    subject_name varchar(30),
+    standard_no number(3),
+    teacher_id varchar(10),
+    day varchar(14) check (day in('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')),
+    start_time varchar(10) not null,
+    end_time varchar(10) not null,
+    primary key(standard_no, subject_name, lecture_no),
+    foreign key(subject_name, standard_no) references subject,
+    foreign key(teacher_id) references teacher
+);
+
+create table working_day
+(
+    day_no number(3),
+    date_ date not null unique,
+    primary key(day_no)
+);
+
+create table attendance
+(
+    day_no number(3),
+    person_id varchar(10),
+    status varchar(15) check (status in('Present', 'Absent')),
+    primary key(day_no, person_id),
+    foreign key(day_no) references working_day,
+    foreign key(person_id) references person
+);
---
+++ dml/person_insert.sql
@@ -0,0 +1,149 @@
+insert into person values(
+    'P0001', 'Meet', 'Ramesh', 'Patel', '101', 'Krishna Crown', 'College Road', 'Nadiad', 387001, 'Male', 1, '11-MAY-2008'
+);
+
+insert into person values(
+    'P0002', 'Maharshi', 'Dipak', 'Jani', '303', 'Aarav Society', 'Canal Road', 'Ghodasar', 380050, 'Male', 1, '25-SEP-2007'
+);
+
+insert into person values(
+    'P0003', 'Amatulla', 'Akbari', 'Gajipurwala', '205', 'Prime Society', 'Punit Road', 'Nadiad', 387002, 'Female', 1, '18-JAN-2006'
+);
+
+insert into person values(
+    'P0004', 'Nihar', 'Vipul', 'Rakholiya', '306', 'Aarav Society', 'Canal Road', 'Ghodasar', 380050, 'Male', 1, '10-SEP-2007'
+);
+
+insert into person values(
+    'P0005', 'Dev', 'Dipesh', 'Patel', '202', 'Royal Heights', 'College Road', 'Nadidad', 387001, 'Male', 1, '10-JAN-2007'
+);
+
+insert into person values(
+    'P0006', 'Het', 'Brijesh', 'Parikh', '54', 'Gokul Society', 'Punit Road', 'Ghodasar', 380050, 'Male', 1, '14-FEB-2009'
+);
+
+insert into person values(
+    'P0007', 'Nensee', 'Kishor', 'Zankat', '48', 'Baleshwar City', 'Lalgebi Road', 'Hathijan', 382445, 'Female', 1, '26-MAR-2010'
+);
+
+insert into person values(
+    'P0008', 'Harshpalsinh', 'Champaksinh', 'Zala', '14', 'Viratnagar Society', 'Lalbhai Road', 'Dhrangadhra', 364554, 'Male', 1, '05-NOV-2009'
+);
+
+insert into person values(
+    'P0009', 'Darshit', 'Dharmesh', 'Vyas', '504', 'Aarav Society', 'Canal Road', 'Ghodasar', 380050, 'Male', 1, '06-JUN-2011'
+);
+
+insert into person values(
+    'P0010', 'Sahil', 'Irfan', 'Vora', '202', 'Royal Heights', 'College Road', 'Nadiad', 387001, 'Male', 1, '10-OCT-2011'
+);
+
+insert into person values(
+    'P0011', 'Yug', 'Nipul', 'Vithani', '45', 'Baleshwar City', 'Lalgebi Road', 'Hathijan', 382445, 'Male', 1, '17-AUG-2012'
+);
+
+insert into person values(
+    'P0012', 'Vasu', 'Piyush', 'Vekariya', '16', 'Viratnagar Society', 'Lalbhai Road', 'Dhrangadhra', 364554, 'Male', 1, '29-NOV-2012'
+);
+
+insert into person values(
+    'P0013', 'Parth', 'Sandeep', 'Vasava', '26', 'Gokul Society', 'Punit Road', 'Ghodasar', 380050, 'Male', 1, '01-AUG-2016'
+);
+
+insert into person values(
+    'P0014', 'Jinesh', 'Vijay', 'Vamja', '17', 'Aarav Society', 'Canal Road', 'Ghodasar', 380050, 'Male', 1, '14-MAR-2016'
+);
+
+insert into person values(
+    'P0015', 'Manish', 'Danabhai', 'Vala', '304', 'Royal Heights', 'College Road', 'Nadiad', 387001, 'Male', 1, '19-SEP-2017'
+);
+
+insert into person values(
+    'P0016', 'Sohel', 'Danishbhai', 'Vahora', '47', 'Baleshwar City', 'Lalgebi Road', 'Hathijan', 382445, 'Male', 1, '04-JUL-2017'
+);
+
+insert into person values(
+    'P0017', 'Prince', 'Sailesh', 'Vaghasiya', '11', 'Gokul Society', 'Punit Road', 'Ghodasar', 380050, 'Male', 1, '06-APR-2016'
+);
+
+insert into person values(
+    'P0018', 'Shrushti', 'Rajesh', 'Vaghasiya', '204', 'Prime Society', 'Punit Road', 'Nadiad', 387002, 'Female', 1, '18-MAR-2016'
+);
+
+insert into person values(
+    'P0019', 'Jimesh', 'Manoj', 'Vadodariya', '404', 'Aarav Society', 'Canal Road', 'Ghodasar', 380050, 'Male', 1, '07-JAN-2017'
+);
+
+insert into person values(
+    'P0020', 'Akshar', 'Anand', 'Vadher', '202', 'Nexus Heights', 'College Road', 'Nadiad', 387001, 'Male', 1, '13-DEC-2016'
+);
+
+insert into person values(
+    'P0021', 'Milan', 'Naresh', 'Vadhel', '303', 'Krishna Crown', 'College Road', 'Nadiad', 387001, 'Male', 1, '11-JAN-2017'
+);
+
+insert into person values(
+    'P0022', 'Shyam', 'Dinesh', 'Vachhani', '401', 'Royal Heights', 'College Road', 'Nadiad', 387001, 'Male', 1, '16-AUG-2016'
+);
+
+insert into person values(
+    'P0023', 'Urvi', 'Rajesh', 'Vasava', '206', 'Prime Society', 'Punit Road', 'Nadiad', 387001, 'Female', 1, '19-JUL-2016'
+);
+
+insert into person values(
+    'P0024', 'Sahil', 'Sanjay', 'Undhad', '103', 'Krishna Crown', 'College Road', 'Nadiad', 387001, 'Male', 1, '30-JAN-2017'
+);
+
+insert into person values(
+    'P0025', 'Khushi', 'Ishwar', 'Umarvanshi', '504', 'Prime Society', 'Punit Road', 'Nadiad', 387001, 'Female', 1, '23-FEB-2017'
+);
+
+insert into person values('P0026','Dhamsania','Dhruvil','Arvindbhai','A1','Sahajanand','A12','Nadiad',387001,'Male',0,'29-DEC-2005');
+
+insert into person values('P0027','Dhanani','Vaibhav','Vijaybhai','A2','Sahajanad','A123','Nadiad',387001,'Male',0,'25-jan-2005');
+
+insert into person values('P0028','Dobariya','Meet','Manojbhai','A3','Sahajanand','A12','Nadiad',387001,'Male',0,'28-feb-2000');
+
+insert into person values('P0029','Dobariya','Vrund','Ghanshayambhai','A4','Vidhyut','B12','Baroda',388812,'Male',0,'12-jan-1998');
+
+insert into person values('P0030','Fadadu','Khanjan','Arvinbhai','B12','Jambuvan','A123','Baroda',388812,'Male',0,'21-dec-2003');
+
+insert into person values('P0031','Faldu','Arpit','Kiritbhai','C12','Vidhya','C2','Nadiad',387001,'Male',0,'22-jan-2000');
+
+insert into person values('P0032','Gabani','Kuldeep','Nareshbhai','Z12','yamuna','J12','Nadiad',387001,'Male',0,'11-nov-1999');
+
+insert into person values('P0033','Gabani','Yash','Niteshbhai','B123','Yamata','X12','Baroda',383880,'Male',0,'01-jan-2001');
+
+insert into person values('P0034','Gajera','Jaimin','Nileshbhai','R12','Vidhyut','C45','Surat',123456,'Male',0,'17-jan-2002');
+
+insert into person values('P0035','Gajera','Kavan','Sanjeevbhai','Y12','Samantha','J12','Nadiad',378001,'Male',0,'12-may-2008');
+
+insert into person values('P0036','Gajera','Rushang','Manojbhai','A2','Samantha','H12','Kheda',345678,'Male',0,'12-jan-2000');
+
+insert into person values('P0037','Gandhi','Nitya','Vijaybhai','C123','Yamuna','H14','Vadodara',345672,'Male',0,'8-feb-2004');
+
+insert into person values('P0038','Ganjawala','Samarth','Sanjaybhai','B12','Vishwa','M12','Nadiad',361312,'Male',0,'12-may-2005');
+
+insert into person values('P0039','Ganvit','Nikunjkumar','Pravinbhai','C65','IPCO','V12','Anand',129023,'Male',0,'17-jan-1999');
+
+insert into person values('P0040','Garala','Mahek','Rameshbhai','V123','Karma','B123','Nadaiad',387001,'Female',0,'1-jan-1992');
+
+insert into person values('P0041','Garasia','Nrushans','Amrutbhai','B122','Vidhya','C90','Annad',370022,'Male',0,'22-may-1998');
+
+insert into person values('P0042','Gohel','Akshay','Rajendrakumar','V15','Amrut','B12','Nadiad',229012,'Male',0,'24-june-1998');
+
+insert into person values('P0043','Gohil','Nana','Gambhirbhai','C45','Krishna','J12','Neshda',387001,'Male',0,'4-jan-2005');
+
+insert into person values('P0044','Gohil','Sumit','Jitendrakumar','Ce044','Krishan','N12','Nadiad',387001,'Male',0,'05-may-1998');
+
+insert into person values('P0045','Gupta','Sanket','Kalpeshbhai','C12','Krishna','H45','Nadiad',387001,'Male',0,'22-feb-2000');
+
+insert into person values ('P0046','Gurjar','Karan','Vijaybhai','V67','Ambika','L12','Kheda',389011,'Male',0,'18-feb-1999');
+
+insert into person values ('P0047','Hadiyal','Mehul','Mohanbhai','C12','Dwarkesh','Mj12','Dwarka',361315,'Male',0,'29-jan-2005');
+
+insert into person values ('P0048','Haraniya','Smith','Niteshkumar','G12','Kanera','K12','Kheda',453617,'Male',0,'22-feb-1998');
+
+insert into person values ('P0049','Husain','Saif','Sahilbhai','H45','Saraswati','L12','Anand',370011,'Male',0,'15-may-1999');
+
+insert into person values ('P0050','Jambudiya','Heet','Ketanbhai','V33','Krishna','C22','Nadiad',370001,'Male',0,'14-may-1997');
